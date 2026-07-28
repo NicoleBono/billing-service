d58c755785b3335d3026d380a214669aff225c7b
nicole <nicolebonomartins@gmail.com>
2026-07-28 19:59:27 -0300

    fix: remover @nestjs/config incompatível com NestJS 11

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { BudgetModule } from './budget/budget.module';
 import { SagaConsumerModule } from './saga/saga-consumer.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    PrismaModule,
-    BudgetModule,
-    SagaConsumerModule,
-  ],
+  imports: [PrismaModule, BudgetModule, SagaConsumerModule],
 })
 export class AppModule {}
